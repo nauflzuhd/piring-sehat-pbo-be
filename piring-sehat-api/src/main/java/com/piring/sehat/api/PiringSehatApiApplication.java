4498c776dae0634ce975ad1b95ce58cdaedeb8fb
iyeppp <miawspeicalist@gmail.com>
2026-05-04 14:46:53 +0700

    Init SpringBoot

---
+++ piring-sehat-api/src/main/java/com/piring/sehat/api/PiringSehatApiApplication.java
@@ -0,0 +1,13 @@
+package com.piring.sehat.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiringSehatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiringSehatApiApplication.class, args);
+	}
+
+}
---
+++ piring-sehat-api/src/test/java/com/piring/sehat/api/PiringSehatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piring.sehat.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiringSehatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
